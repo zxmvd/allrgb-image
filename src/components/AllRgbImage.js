4ddc8aed6e659d433407043e9a34d5e89db55e04
zxmvd <zx_mailhot@hotmail.com>
2021-05-17 16:15:39 +1000

    react boiler plate created

---
+++ src/components/AllRgbImage.js
@@ -0,0 +1,16 @@
+import {useEffect} from 'react'
+import { renderAllRgbImg } from '../services/renderAllRgbImg'
+
+const AllRgbImage = ()=>{
+
+    // render image once component loaded
+    useEffect(()=>{
+        renderAllRgbImg()
+    },[])
+
+    return(
+            <canvas id="all_color_img" className='canvas'></canvas>
+    )
+}
+
+export default AllRgbImage
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import logo from '../ehg-reverseo.png';
+
+const Header = ()=>{
+    return (
+        <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          EHG <br/>Coding Chllenge
+        </p>
+
+        </div>
+    )
+}
+
+export default Header
